5a90caacc22382e15944e13667b8143206ee9468
Ahmed Ghaith <a.wesam2300@nu.edu.eg>
2026-01-18 11:58:55 +0200

    c

--- Model Integration.py
+++ Model Integration.py
@@ -10,15 +10,15 @@ rf = joblib.load("rf_model.pkl")      # RandomForestClassifier
 knn = joblib.load("knn_model.pkl")    # KNeighborsClassifier
 
 scaler = joblib.load("scaler.pkl")    # StandardScaler
-encoder = joblib.load("label_encoder.pkl")  # LabelEncoder
 
 models = [svm, lr, rf, knn]
 model_names = ["SVM", "Logistic Regression", "Random Forest", "kNN"]
 
 # Accuracy-based weights
-weights = [0.64, 0.63, 0.5925, 0.6287]  # match models list order
+weights = [0.64, 0.6808, 0.5925, 0.6287]  # match models list order
 
-label_names = ['Home Win', 'Draw', 'Away Win']  
+# Manual label mapping
+label_names = ['Home Win', 'Draw', 'Away Win']  # 0,1,2
 
 feature_names = [
     'Half Time Home Goals',
@@ -37,7 +37,11 @@ for f in feature_names:
 if st.button("Predict"):
     # Create DataFrame
     X = pd.DataFrame([inputs], columns=feature_names).astype(float)
-    X_scaled = scaler.transform(X)
+
+    X_imp = X
+
+    # ---- Scale features ----
+    X_scaled = scaler.transform(X_imp)
 
     st.subheader("Individual Model Predictions")
     preds = []
@@ -45,10 +49,10 @@ if st.button("Predict"):
 
     # Get predictions and weighted votes
     for name, model, w in zip(model_names, models, weights):
-        pred = model.predict(X_scaled)[0]
+        pred = model.predict(X_scaled)[0]  # imputed + scaled input
         preds.append(pred)
         pred_weights.append((pred, w))
-        st.write(f"{name}: {encoder.inverse_transform([pred])[0]} ({label_names[pred]})")
+        st.write(f"{name}: {label_names[pred]}")
 
     # ---- Weighted Voting ----
     vote_counts = {}
@@ -59,7 +63,7 @@ if st.button("Predict"):
     final = max(vote_counts, key=vote_counts.get)
 
     st.subheader("Final Verdict")
-    st.write(f"{encoder.inverse_transform([final])[0]} ({label_names[final]})")
+    st.write(f"{label_names[final]}")
 
     # ---- Confidence ----
     total_weight = sum(vote_counts.values())
